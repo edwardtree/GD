43e1e65bd81266167c9b808229b002df47d7cfea
edwardtree <edwardsmalltree@yahoo.com.tw>
2014-08-13 17:19:10 +0800

    initial input and qsort

--- bundling.c
+++ bundling.c
@@ -1,49 +1,101 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
-#define N 10 
+
+#define N 100 
 
 typedef struct {
 	int x;
 	int y;
 } vertex;
 
+vertex node_arr[N];
+
+
+int cmp(const void *p, const void *q){
+	double *a= (double*)p;
+	double *b= (double*)q;
+	if (*a<*b) return -1;
+	if (*a==*b) return 0;
+	return 1;
+}
+
+
+
+
+double lengh[N]={0.0};
+
+double edgelengh( int vertex_a_x, int vertex_a_y, int vertex_b_x,int vertex_b_y){
+	
+	double num;
+
+	num=(vertex_a_x-vertex_b_x)*(vertex_a_x-vertex_b_x)+(vertex_a_y-vertex_b_y)*(vertex_a_y-vertex_b_y);
+	num=pow(num,0.5);
+	printf("num : %f\n",num);
+
+	return num;
+} 
+
 
 int main(){
-	int x,y,i,j,n;
-	int graph[N][N];
-
+	int x,y,i,j,n,m;
+	int graph[N][N]={0};
+	
 /*----------input graph-----------*/
 
-	scanf("%d",&n);
-	vertex node_arr[n];
-	for( i = 0 ; i < N ; i++ )
-	  for( j = 0 ; j < N ; j++ )
-	    graph[i][j]=0;
+	//vertices
+	printf("total vertices number : ");
+	scanf("%d",&n); //n = total number of vertices
 	
 	for( i = 0 ; i < n ; i++ ){
-	  scanf("%d %d",&x,&y);
-	  node_arr[i].x = x,node_arr[i].y = y;
+	  printf("coordinates_x , coordinates_y  : ");
+	  scanf("%d %d",&node_arr[i].x,&node_arr[i].y);
 	}
-	printf("---------------------------");
+
+	printf("---------------------------\n");
 	for( i = 0 ; i < n; i++ )
 	    printf("%d %d\n", node_arr[i].x, node_arr[i].y );
-	    //if( j == N-1)printf("\n");
+
+	//Edges
 	
-/*
-	for( i = 0 ; i < N ; i++ )
-	  for( j = 0 ; j < N ; j++ ){
-	    printf("%d", graph[i][j]);
-	    if( j == N-1)printf("\n");
+	for( i = 0 ; i < n ; i++ )
+	  for( j = 0 ; j < n ; j++ ){
+	    graph[i][j]=0; 
+	    if(i==j)graph[i][j]=1;
 	}
-*/
+
+	printf("total edges number :");        
+	scanf("%d",&m);
+
+	for( i = 0 ; i < m ; i++ ){
+		printf("edge %d ( vertex x , vertex y ) = ",i);
+		scanf("%d %d",&x,&y);
+		lengh[i]=edgelengh(node_arr[x].x,node_arr[x].y,node_arr[y].x,node_arr[y].y);
+		printf("lengh : %f\n",lengh[i]);
+		graph[x][y]=graph[y][x] = 1;	
+
+	}
+
+	//printf(" total vertice are %d\n", n );
+	for( i = 0 ; i < n ; i++ )
+	  for( j = 0 ; j < n ; j++ ){
+	    //printf("%d" ,graph[i][j]);
+	    if(j == n-1) printf("\n");
+	}
 
 
+	qsort(lengh,m,sizeof(double),cmp);
 
-
-
+	
+	for( i = 0; i<m ; i++ ) printf("lengh[%d] =%f\n",i ,lengh[i]);
+	
 
 /*----------minimize vertice to bundling vertice-----------*/
+
+
+
+
 
 
 
@@ -56,7 +108,7 @@ int main(){
 
 
 	
-  return 0
+  return 0;
 
 
 }
